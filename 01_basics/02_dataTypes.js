46a4c71af5498f339e9481a3e7cbc243fbd54b06
bycodeinfotech001 <141473935+bycodeinfotech001@users.noreply.github.com>
2023-12-10 09:31:46 +0000

    new

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,6 +2,28 @@
 
 // alert(5 + 5) We are using Node Js, not browser
  
-console.log(3 + 9)
+console.log(3 
+    +
+     3) // code readability should be high
 
 console.log("yash")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof "yash"); // string
