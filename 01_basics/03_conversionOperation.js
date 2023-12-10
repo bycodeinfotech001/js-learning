3aaac866bd197c96b0340432e3c508d15874e638
bycodeinfotech001 <141473935+bycodeinfotech001@users.noreply.github.com>
2023-12-10 10:00:05 +0000

    new

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,12 @@
-let score = "yash"
+let score = "hitesh"
 
-// console.log(typeof score)
-// console.log(typeof (score))
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
 
 // "33" => 33
 // "33abc" => NaN
@@ -19,3 +20,16 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let nagValue = -value
+// console.log(nagValue)
